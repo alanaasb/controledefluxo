e281a5af9070fb2b2e5f0f8301a533f2d3a94b12
alana <alanabrizola@gmail.com>
2024-07-17 12:16:57 +0200

    desafiofinnalizado

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+
+
+public class ParametrosInvalidosException extends Exception{
+
+    public static final long serialVersionUID = 1L;
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
